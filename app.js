507f5f2f8644be53b6dd38db2f0a5c043f39db22
PavelAkulovich <pasha860@mail.ru>
2021-12-01 19:29:08 +0300

    fix: change selectors to classes according to rule 3.4

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    deleteButtonImg.className='btn-img';
+    listItem.className='item';
     label.innerText=taskString;
     label.className='task';
 
